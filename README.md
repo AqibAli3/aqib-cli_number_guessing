8a1fdad06e85f399ded9a4b64fda97f7f5f87a8f
AqibAli3 <147250919+AqibAli3@users.noreply.github.com>
2024-03-26 01:24:49 +0500

    push my project

---
+++ README.md
@@ -0,0 +1,2 @@
+"# aqib-cli_number_guessing" 
+"# aqib-cli_number_guessing" 
---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+//you can open this console if you want see what is randomNumber exactly for safe your time
+// console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess A number between 1 to 10: ",
+    }
+]);
+if (answers.userGuessNumber === randomNumber)
+    console.log("Your answer is number: " + randomNumber + ", Congratulation! your answer is correct. ");
+// i have added a function to return correct random number also if user putted wrong number
+else {
+    console.log("Sorry dear! your answer is wrong, Correct Number is " + randomNumber + ", Please try Again.");
+}
